6e1bc9e00308a318f46dd729420ab1d3bbe8fb02
ridi-lkw <lkw@ridi.com>
2019-12-11 13:48:48 +0900

    Make benchmark for the HyperLogLogPlusPlus

--- benchmark/hyperloglog_benchmark.py
+++ benchmark/hyperloglog_benchmark.py
@@ -2,16 +2,17 @@
 Performance and accuracy of HyperLogLog
 '''
 import time, logging, random
-from datasketch.hyperloglog import HyperLogLog
+from datasketch.hyperloglog import HyperLogLog, HyperLogLogPlusPlus
 
 logging.basicConfig(level=logging.INFO)
 
 # Produce some bytes
-int_bytes = lambda x : ("a-%d-%d" % (x, x)).encode('utf-8')
+int_bytes = lambda x: ("a-%d-%d" % (x, x)).encode('utf-8')
 
-def run_perf(card, p):
-    h = HyperLogLog(p=p)
-    logging.info("HyperLogLog using p = %d " % p)
+
+def run_perf(card, p, _class):
+    logging.info(f'{_class.__name__} using p = {p}')
+    h = _class(p=p)
     start = time.clock()
     for i in range(card):
         h.update(int_bytes(i))
@@ -20,9 +21,9 @@ def run_perf(card, p):
     return duration
 
 
-def run_acc(size, seed, p):
-    logging.info("HyperLogLog using p = %d " % p)
-    h = HyperLogLog(p=p)
+def run_acc(size, seed, p, _class):
+    logging.info(f'{_class.__name__} using p = {p}')
+    h = _class(p=p)
     s = set()
     random.seed(seed)
     for i in range(size):
@@ -32,35 +33,39 @@ def run_acc(size, seed, p):
     perr = abs(float(len(s)) - h.count()) / float(len(s))
     return perr
 
-ps = range(4, 17)
-output = "hyperloglog_benchmark.png"
 
-logging.info("> Running performance tests")
-card = 5000
-run_times = [run_perf(card, p) for p in ps]
+def run_bench(card, size, _class):
+    ps = range(4, 17)
+    output = f"{_class.__name__}_benchmark.png"
 
-logging.info("> Running accuracy tests")
-size = 5000
-errs = [run_acc(size, 1, p) for p in ps]
+    logging.info("> Running performance tests")
+    run_times = [run_perf(card, p, _class) for p in ps]
 
-logging.info("> Plotting result")
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-fig, axe = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
-ax = axe[1]
-ax.plot(ps, run_times, marker='+')
-ax.set_xlabel("P values")
-ax.set_ylabel("Running time (sec)")
-ax.ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
-ax.set_title("HyperLogLog performance")
-ax.grid()
-ax = axe[0]
-ax.plot(ps, errs, marker='+')
-ax.set_xlabel("P values")
-ax.set_ylabel("Error rate in cardinality estimation")
-ax.set_title("HyperLogLog accuracy")
-ax.grid()
+    logging.info("> Running accuracy tests")
+    errs = [run_acc(size, 1, p, _class) for p in ps]
 
-fig.savefig(output)
-logging.info("Plot saved to %s" % output)
+    logging.info("> Plotting result")
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    fig, axe = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
+    ax = axe[1]
+    ax.plot(ps, run_times, marker='+')
+    ax.set_xlabel("P values")
+    ax.set_ylabel("Running time (sec)")
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+    ax.set_title(f"{_class.__name__} performance")
+    ax.grid()
+    ax = axe[0]
+    ax.plot(ps, errs, marker='+')
+    ax.set_xlabel("P values")
+    ax.set_ylabel("Error rate in cardinality estimation")
+    ax.set_title(f"{_class.__name__} accuracy")
+    ax.grid()
+
+    fig.savefig(output)
+    logging.info("Plot saved to %s" % output)
+
+
+run_bench(5000, 5000, HyperLogLog)
+run_bench(5000, 5000, HyperLogLogPlusPlus)
